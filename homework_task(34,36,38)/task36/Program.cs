d21694cad447d606fe17e9f0c74cd169a67b1d56
Koryun <saribekyan.koryun@mail.ru>
2023-02-12 22:48:21 +0400

    finish343638

---
+++ homework_task(34,36,38)/task36/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+//  Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+Console.Clear();
+int [] array = new int [10];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 11);
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+int summa = 0;
+for (int i = 1; i < array.Length; i=i+2)
+    {
+    summa = summa + array[i];
+    }
+Console.WriteLine($"Сумма элементов,стоящих на нечётных позициях: {summa} ");
+
---
+++ homework_task(34,36,38)/task38/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте массив целых чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3, 7, 22, 2, 78] -> 76
+
+Console.Clear();
+int [] array = new int [10];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 100);
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+int max = 0;
+int min = 100;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+Console.WriteLine($"Paзницa между максимальным и минимальным элементов массива: {max-min}");
