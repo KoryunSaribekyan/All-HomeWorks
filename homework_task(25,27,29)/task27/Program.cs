29b9ea032496eae1733296f7a8792634eb5c0025
Koryun <saribekyan.koryun@mail.ru>
2023-02-12 16:15:30 +0400

    end

---
+++ homework_task(25,27,29)/task27/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+    
+//     int Dlina = Convert.ToString(n).Length;
+//     int result = 0;
+
+//     for (int i = 0; i < Dlina; i++)
+//     {
+//       result = result + (n % 10);
+//       n = n / 10;
+//     }
+
+// Console.WriteLine(result);
+
+Console.Write("Введите число: ");
+string? stroka = Console.ReadLine();
+int summa = 0;
+for(int i = 0; i<stroka.Length; i++)
+{
+summa = summa + Convert.ToInt32(stroka[i].ToString());
+}
+Console.WriteLine(summa);
+
+
