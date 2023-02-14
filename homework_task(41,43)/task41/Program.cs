81d2dda0f2983d2c8466b7167bed8c683b8fc8f6
Koryun <saribekyan.koryun@mail.ru>
2023-02-14 11:51:29 +0400

    task41_43

---
+++ homework_task(41,43)/task41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+Console.WriteLine("Введите числа: ");
+int count = 0;
+for(int i=0;i<7;i++)
+{
+    Console.Write("");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+if (n>0)
+{
+    count++;
+}
+}
+Console.WriteLine($"Количество чисел больше 0: {count}");
